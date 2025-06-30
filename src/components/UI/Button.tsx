750f04fff055ffeab1cdf0b0964edb013c5ed2dd
sakethguptha45 <145485094+sakethguptha45@users.noreply.github.com>
2025-06-30 14:37:46 -0400

    Updated Button.tsx

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import { LucideIcon } from 'lucide-react';
+import { DivideIcon as LucideIcon } from 'lucide-react';
 
 export interface ButtonProps {
   children: React.ReactNode;
